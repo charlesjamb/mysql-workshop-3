0651b1a07b07d10c01d59a3ec7d3a19d37bb6541
charlesjamb <charles.jambon@gmail.com>
2016-10-18 20:08:14 -0400

    exercise 2

---
+++ exercise2.sql
@@ -0,0 +1,5 @@
+mysql -u root -p decodemtl_addressbook < ~/workspace/decodemtl/sqlWorkshop3/data/import-account.sql
+
+ mysql -u root -p decodemtl_addressbook < ~/workspace/decodemtl/sqlWorkshop3/data/import-addressbook.sql
+
+mysql -u root -p decodemtl_addressbook < ~/workspace/decodemtl/sqlWorkshop3/data/import-entry.sql
